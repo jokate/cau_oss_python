cb56d57019c4ce3458694a2e09245b84b3d3bfd6
jokate <kkkk4017@naver.com>
2023-03-23 18:27:06 +0900

    Update prac1.py

--- prac1.py
+++ prac1.py
@@ -2,7 +2,18 @@ def init():
     width = float(input())
     length = float(input())
     height = float(input())
-    return width * length * height
+    return width + length + height
+
+def check() :
+    checkval = init()
+    if(checkval <= 80.0):
+        return 5000
+    elif(checkval <= 100):
+        return 8000
+    elif (checkval <= 120.0):
+        return 10000
+    elif (checkval <= 160.0):
+        return 12000
 
 
-print("Total Volume : ", init())
+print("Total Volume : ", check())
